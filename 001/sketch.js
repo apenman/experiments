d8c3ac31a191f6ad62280786ba6e900911a333f9
Alex <apenman427@gmail.com>
2017-05-24 00:21:50 -0700

    Added sol as first experiment

--- 001/sketch.js
+++ 001/sketch.js
@@ -1,5 +1,9 @@
-var colors;
-var canvas;
+var xPoints = [];
+var yPoints = [];
+var numPoints = 50;
+var currLineStep = 0.0;
+var i = 0;
+var j = 0;
 
 function setup() {
   // Set canvas
@@ -7,15 +11,52 @@ function setup() {
   // May cause issues with not seeing to the very bottom of sketch since vertical scroll is disabled
   canvas = createCanvas(windowWidth, windowHeight - 38);
   canvas.class('sketch');
-  colors = new Color();
+  background(255);
+
+  for (var i = 0; i < numPoints; i++) {
+    xPoints[i] = random(windowWidth - 100);
+    yPoints[i] = random(windowHeight - 50);
+  }
+
+  i = 0;
+  j = 1;
+}
+
+function drawPoints() {
+  for (var d = 0; d < numPoints; d++) {
+    point(xPoints[d], yPoints[d])
+  }
+}
+
+function animateLine() {
+  // Use lerp() to calc point between x's and y's
+  var newX = lerp(xPoints[i], xPoints[j], currLineStep);
+  var newY = lerp(yPoints[i], yPoints[j], currLineStep);
+
+  stroke(0);
+  strokeWeight(0.25);
+
+  line(xPoints[i], yPoints[i], newX, newY);
+
+  if (currLineStep < 1.0)
+    currLineStep = currLineStep + 0.1 > 1.0 ? 1.0 : currLineStep + 0.1;
+  else {
+    if (j < numPoints - 1) {
+      j++;
+    } else {
+      j = 0;
+      i++;
+      // END OF ANIMATION
+      if (i == numPoints) {
+        console.log("DONE!");
+        noLoop();
+      }
+    }
+    currLineStep = 0.0;
+  }
 }
 
 function draw() {
-  background(colors.r, colors.g, colors.b);
-}
-
-function Color() {
-  this.r = 255;
-  this.g = 100;
-  this.b = 255;
+  drawPoints();
+  animateLine();
 }
