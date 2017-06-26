1b673d26e5709c507c9b005714ed4cf95e375422
Alex <apenman427@gmail.com>
2017-06-26 00:28:52 -0700

    Rotations

--- 006/sketch.js
+++ 006/sketch.js
@@ -1,7 +1,15 @@
+// TODO:
+// 1. Center the drawing
+// 2. Add left and right possibility to face left or right
+// 3. Randomly decide direction of first up or down
+
 var canvas, canvasWidth, canvasHeight;
 // Space in between lines
 var spacing;
-var thing;
+var things;
+var triLength;
+var debug;
+var numTriangles;
 function setup() {
   // Set canvas
   // Subtract from windowHeight to account for header. Header is 40px so we subtract 38px in order to fill screen
@@ -13,20 +21,12 @@ function setup() {
 
   background(255);
 
+  triLength = 200;
   spacing = 20;
-  things = new Thingy(canvasWidth/3, canvasHeight/2, 1);
-  thing2 = new Thingy(things.rightPoint.x + spacing/2, things.rightPoint.y, -1);
-  thing3 = new Thingy(thing2.leftPoint.x + spacing/2, thing2.leftPoint.y, 1);
-  thing4 = new Thingy(thing3.rightPoint.x + spacing/2, thing3.rightPoint.y, -1);
-
-  things.plotThingy();
-  thing2.plotThingy();
-  thing3.plotThingy();
-  thing4.plotThingy();
+  initTriangles();
 }
 
-function draw() {
-}
+function draw() {}
 
 function windowResized() {
   canvasWidth = windowWidth;
@@ -34,24 +34,30 @@ function windowResized() {
   resizeCanvas(windowWidth, canvasHeight);
 }
 
+function initTriangles() {
+  numTriangles = random([3,4,5,6,7]);
+  things = [];
+  things.push(new Thingy((canvasWidth/numTriangles), canvasHeight/2, dir));
+  for(var i = 1; i < numTriangles; i++) {
+    var dir;
+    i%2==0?dir="down":dir="up";
+    things.push(new Thingy((canvasWidth/numTriangles) + (triLength/2*i) + (spacing/2*i), canvasHeight/2, dir));
+  }
+
+  for(var i = 0; i < things.length; i++) {
+    things[i].plotThingy();
+  }
+}
+
 function mouseClicked() {
   background(255);
-  things.plotThingy();
-  thing2.plotThingy();
-  thing3.plotThingy();
-  thing4.plotThingy();
+  initTriangles()
 }
 
 function Thingy(x, y, dir) {
   this.direction = dir;
-  this.length = 200;
-  this.peak = new Point(x, y);
-  this.midpoint = new Point(this.peak.x, this.peak.y + (dir * sqrt(sq(this.length)-sq(this.length/2))));
-  this.height = dist(this.peak.x, this.peak.y, this.midpoint.x, this.midpoint.y);
-  this.leftPoint = new Point(this.midpoint.x-(dir * (this.length/2)), this.midpoint.y);
-  this.rightPoint = new Point(this.midpoint.x+(dir * (this.length/2)), this.midpoint.y);
-  console.log("HEIGHT " + this.height);
-
+  this.middle = new Point(x, y);
+  this.height = sqrt(sq(triLength)-sq(triLength/2));
 
   // Store the three points for the first phase
   // Calculate three new points for new iteration by manually adjusting for the space, get the length manually between the new points
@@ -62,23 +68,36 @@ function Thingy(x, y, dir) {
     var left, right, midpoint;
     var offset, newlength;
 
+    push();
+    // Move origin to middle of triangle
+    // We are now operating with the middle point as the origin
+    translate(this.middle.x, this.middle.y);
+    if(this.direction == "up"){rotate(radians(180))}
+
     for(var i = 0; i < 6; i++){
       offset = spacing * i;
       // Handle case of first lines
       if(i == 0) {
-        newlength = this.length;
+        newlength = triLength;
       }
       else {
-        newlength = sqrt(sq(this.length/2 - offset) + sq(this.height-offset));
+        newlength = sqrt(sq(triLength/2 - offset) + sq(this.height-offset));
       }
 
-      left = new Point(this.midpoint.x-(newlength/2), this.midpoint.y);
-      right = new Point(this.midpoint.x+(newlength/2), this.midpoint.y);
-      // ellipse(this.peak.x, this.peak.y, 5);
-      // ellipse(left.x, left.y, 5);
-      // ellipse(right.x, right.y, 5);
-      line(this.peak.x, this.peak.y + (dir * offset), left.x, left.y);
-      line(this.peak.x, this.peak.y + (dir * offset), right.x, right.y);
+      left = new Point(-newlength/2, this.height/2);
+      right = new Point(newlength/2, this.height/2);
+
+      line(0, -(this.height/2) + offset, left.x, left.y);
+      line(0, -(this.height/2) + offset, right.x, right.y);
+
+      if(debug) {
+        line(0, -(this.height/2) + offset, 0, this.height/2);
+        ellipse(0, -(this.height/2) + (this.height/2));
+        ellipse(0, 0, 5);
+        ellipse(left.x, left.y, 5);
+        ellipse(right.x, right.y, 5);
+      }
     }
+    pop();
   }
 }
