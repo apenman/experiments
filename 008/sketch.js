8298a40d1d6ea32b2129b4d96e046c12f768cfed
Alex <apenman427@gmail.com>
2019-06-30 18:53:56 -0700

    Unknown pleasures

--- 008/sketch.js
+++ 008/sketch.js
@@ -1,5 +1,6 @@
 var canvas, canvasWidth, canvasHeight;
 var step;
+var lines;
 
 function setup() {
   // Set canvas
@@ -10,16 +11,62 @@ function setup() {
   canvasHeight = windowHeight - 38;
   canvas.class('sketch');
 
+
   background(255);
-  stroke(145);
-  step = 80;
+  stroke(1);
   noLoop();
 }
 
 function draw() {
-  // context.stroke();
-  for(var x = 0; x < canvasWidth; x += step) {
-    for(var y = 0; y < canvasHeight; y += step) {
+  // tiledLines();
+  unknownPleasures();
+}
+
+// Still need to tweak values so that it looks nicer
+function unknownPleasures() {
+  step = 20;
+  lines = [];
+  generateLinePoints();
+  drawUnknownLines();
+}
+
+function generateLinePoints() {
+  //increase of decrease to change spread
+  var offsetFromCenter = 200;
+  for (var i = step; i <= canvasHeight - step; i += step) {
+    var points = [];
+    for (var j = step; j <= canvasWidth - step; j += step) {
+      var distanceToCenter = Math.abs(j - canvasWidth / 2);
+      var variance = Math.max(canvasWidth / 2 - offsetFromCenter - distanceToCenter, 0);
+      var displace = Math.random() * variance / 2 * -1;
+      var point = {
+        x: j,
+        y: i + displace
+      };
+
+      points.push(point)
+    }
+    lines.push(points);
+  }
+}
+
+function drawUnknownLines() {
+  for (var i = 5; i < lines.length; i++) {
+    beginShape();
+
+    for (var j = 0; j < lines[i].length; j++) {
+      console.log(lines[i][j].x, lines[i][j].y);
+      curveVertex(lines[i][j].x, lines[i][j].y);
+    }
+    endShape();
+  }
+}
+
+function tiledLines() {
+  step = 80;
+  
+  for (var x = 0; x < canvasWidth; x += step) {
+    for (var y = 0; y < canvasHeight; y += step) {
       drawLines(x, y, step, step);
     }
   }
